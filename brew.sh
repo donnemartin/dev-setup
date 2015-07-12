826d5f4146dd1bfb0b49b7863da5e39129cbd5d7
Donne Martin <donne.martin@gmail.comexport>
2015-07-12 10:02:37 -0400

    brew.sh: Added git-flow.

--- brew.sh
+++ brew.sh
@@ -86,6 +86,7 @@ brew install dark-mode
 #brew install exiv2
 brew install git
 brew install git-lfs
+brew install git-flow
 brew install imagemagick --with-webp
 brew install lua
 brew install lynx
