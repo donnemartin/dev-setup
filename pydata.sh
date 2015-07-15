1e0f2225ca6dcb2927007891c0612af0e0dc59ef
Donne Martin <donne.martin@gmail.com>
2015-07-15 06:06:22 -0400

    pydata.sh: Source virtualenvwrapper from .extra.

--- pydata.sh
+++ pydata.sh
@@ -8,6 +8,7 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.pydata` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+echo "------------------------------"
 echo "Setting up pip."
 
 # Install pip
@@ -17,23 +18,24 @@ easy_install pip
 # Virtual Enviroments                                                         #
 ###############################################################################
 
+echo "------------------------------"
 echo "Setting up virtual environments."
 
 # Install virtual environments
 pip install virtualenv
 pip install virtualenvwrapper
 
-#echo "Source virtualenvwrapper from ~/.extra"
+echo "------------------------------"
+echo "Source virtualenvwrapper from ~/.extra"
 
-#EXTRA_PATH="~/.extra"
-#echo $EXTRA_PATH
-#echo "" >> $EXTRA_PATH
-#echo "" >> $EXTRA_PATH
-#echo "# Source virtualenvwrapper" >> $EXTRA_PATH
-#echo "# Added by .pydata" >> $EXTRA_PATH
-#echo "export WORKON_HOME=~/.virtualenvs" >> $EXTRA_PATH
-#echo "source /usr/local/bin/virtualenvwrapper.sh" >> $EXTRA_PATH
-#source $EXTRA_PATH
+EXTRA_PATH=~/.extra
+echo $EXTRA_PATH
+echo "" >> $EXTRA_PATH
+echo "" >> $EXTRA_PATH
+echo "# Source virtualenvwrapper, added by pydata.sh" >> $EXTRA_PATH
+echo "export WORKON_HOME=~/.virtualenvs" >> $EXTRA_PATH
+echo "source /usr/local/bin/virtualenvwrapper.sh" >> $EXTRA_PATH
+source $EXTRA_PATH
 
 ###############################################################################
 # Python 2 Virtual Enviroment                                                 #
@@ -79,6 +81,7 @@ pip install scikit-learn
 pip install "ipython[all]"
 pip install boto
 
+echo "------------------------------"
 echo "Script completed."
 echo "Usage: workon py2-data for Python2"
 echo "Usage: workon py3-data for Python3"
