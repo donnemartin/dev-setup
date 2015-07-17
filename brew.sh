36b22bee46ed2c69c322ae3b91b8ddb0c9d09385
Donne Martin <donne.martin@gmail.com>
2015-07-17 06:14:11 -0400

    brew.sh: Added note about updating your bash shell, referencing the README.

--- brew.sh
+++ brew.sh
@@ -26,11 +26,11 @@ brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
 brew install gnu-sed --with-default-names
 # Install Bash 4.
-# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
-# running `chsh`.
 brew install bash
 brew tap homebrew/versions
 brew install bash-completion2
+# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
+# running `chsh`.  See the README.
 
 # Install `wget` with IRI support.
 brew install wget --with-iri
@@ -105,8 +105,6 @@ brew install zopfli
 brew install python
 brew install python3
 brew install mysql
-
-
 
 # Remove outdated versions from the cellar.
 brew cleanup
