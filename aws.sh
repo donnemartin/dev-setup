9bd70eb5f24c34350cd62b0a1b57cb11b6a0f85d
Donne Martin <donne.martin@gmail.com>
2015-07-18 08:15:33 -0400

    aws.sh: Initial commit.

---
+++ aws.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# ~/.pydata
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.pydata` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+echo "------------------------------"
+echo "Setting up AWS."
+
+echo "------------------------------"
+echo "Source virtualenvwrapper from ~/.extra"
+source ~/.extra
+
+###############################################################################
+# Python 2 Virtual Enviroment                                                 #
+###############################################################################
+
+echo "------------------------------"
+echo "Updating py2-data virtual environment with AWS modules."
+
+# Create a Python2 data environment
+# If this environment already exists from running pydata.sh,
+# it will not be overwritten
+mkvirtualenv py2-data2
+workon py2-data2
+
+pip install boto
+pip install awscli
+pip install mrjob
+pip install s3cmd
+
+echo "Script completed."
