c1bdae75a77bc3a0e831efd2826bd08961d20985
Donne Martin <donne.martin@gmail.com>
2015-07-19 13:33:54 -0400

    brew.sh: Install Homebrew if it doesn't exist.

--- brew.sh
+++ brew.sh
@@ -8,6 +8,13 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+# Check for Homebrew,
+# Install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
 # Make sure we’re using the latest Homebrew.
 brew update
 
