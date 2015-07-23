22fcd438eaeea35651ab76430725a404cfbad513
Donne Martin <donne.martin@gmail.com>
2015-07-23 07:11:25 -0400

    aws.sh: Automatically install Homebrew if it is not installed.  Brew is needed to install Spark.

--- aws.sh
+++ aws.sh
@@ -55,8 +55,12 @@ pip install awscli
 # System-Wide Packages                                                        #
 ###############################################################################
 
-# If you need Homebrew, run the following:
-#ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Check for Homebrew,
+# Install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
 # Make sure we’re using the latest Homebrew.
 brew update
