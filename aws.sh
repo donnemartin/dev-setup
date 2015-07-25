bc8b258e00795837a8eb16a50bc279a84cf81c0d
Donne Martin <donne.martin@gmail.com>
2015-07-25 05:34:17 -0400

    aws.sh: Prevent running the script with sudo.

--- aws.sh
+++ aws.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-# ~/.pydata
+# ~/aws.sh
 
-# Ask for the administrator password upfront
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until `.pydata` has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+# Removed user's cached credentials
+# This script might be run with .dots, which uses elevated privileges
+sudo -K
 
 echo "------------------------------"
 echo "Setting up AWS."
