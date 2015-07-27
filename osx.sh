d5e18040e8fc88be624c233827e230160851cadd
Donne Martin <donne.martin@gmail.com>
2015-07-27 01:56:25 -0400

    osx.sh: Updated outdated script name in comments.

--- osx.sh
+++ osx.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-# ~/.osx — https://mths.be/osx
+# ~/osx.sh — Originally from https://mths.be/osx
 
 # Ask for the administrator password upfront
 sudo -v
 
-# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+# Keep-alive: update existing `sudo` time stamp until `osx.sh` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 ###############################################################################
