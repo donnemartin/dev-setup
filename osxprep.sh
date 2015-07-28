cd21142d9d42106bb1265fd4ee45b9ff5185114c
Donne Martin <donne.martin@gmail.com>
2015-07-28 08:38:23 -0400

    Fixed script name within a comment.

--- osxprep.sh
+++ osxprep.sh
@@ -3,7 +3,7 @@
 # Ask for the administrator password upfront
 sudo -v
 
-# Keep-alive: update existing `sudo` time stamp until `.osxprep` has finished
+# Keep-alive: update existing `sudo` time stamp until `osxprep.sh` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Step 1: Update the OS and Install Xcode Tools
