1556810327ff7cec4acbd01e82d5da804e4c38c2
wildone <max.barrass@gmail.com>
2016-06-22 00:36:08 +1000

    Create java.sh

---
+++ java.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Check for Homebrew,
+# Install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Install Cask
+brew install caskroom/cask/brew-cask
+brew tap caskroom/versions
+
+brew cask install --appdir="~/Applications" java
+brew cask install --appdir="~/Applications" java7
+brew cask install --appdir="~/Applications" Caskroom/versions/intellij-idea-ce
+brew cask install --appdir="~/Applications" android-studio
+brew cask install --appdir="~/Applications" intellij-idea
+#brew cask install --appdir="~/Applications" jedit #sha256 mismatch
+brew install groovy
+
+brew install android-sdk
+
+# Remove outdated versions from the cellar.
+brew cleanup
