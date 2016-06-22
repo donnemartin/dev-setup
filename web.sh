b237c463d4730b008d08979144a085e004df8f3f
Max Barrass <max.barrass@gmail.com>
2016-06-22 11:50:25 +1000

    update we role with new tools

--- web.sh
+++ web.sh
@@ -18,7 +18,7 @@ fi
 # Make sure we’re using the latest Homebrew.
 brew update
 
-brew install node
+brew install node npm
 
 # Remove outdated versions from the cellar.
 brew cleanup
@@ -27,5 +27,7 @@ npm install -g coffee-script
 npm install -g grunt-cli
 npm install -g jshint
 npm install -g less
+npm install -g repoman
+npm install -g gulp
 
 #gem install jekyll
