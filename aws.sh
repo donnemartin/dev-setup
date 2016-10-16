801deb89ed0b306693ccfef5c0564e3ebe843871
D Condrey <davidcondrey@me.com>
2016-10-16 04:43:54 -0700

    removes redundancies and unifies  brew install

--- aws.sh
+++ aws.sh
@@ -74,13 +74,7 @@ pip install awscli
 
 # Check for Homebrew,
 # Install if we don't have it
-if test ! $(which brew); then
-  echo "Installing homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-
-# Make sure we’re using the latest Homebrew.
-brew update
+source brew.sh; install
 
 brew install apache-spark
 
--- brew.sh
+++ brew.sh
@@ -8,18 +8,19 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Check for Homebrew,
-# Install if we don't have it
-if test ! $(which brew); then
-  echo "Installing homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-
-# Make sure we’re using the latest Homebrew.
-brew update
-
-# Upgrade any already-installed formulae.
-brew upgrade --all
+# Install or upgrade Homebrew
+install() {
+    # Install if we don't have it
+    if test ! $(which brew); then
+        echo "Installing homebrew..."
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    else
+        # Make sure we’re using the latest Homebrew.
+        brew update
+        # Upgrade any already-installed formulae.
+        brew upgrade --all
+    fi
+}
 
 # Install GNU core utilities (those that come with OS X are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
