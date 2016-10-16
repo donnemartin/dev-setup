adcbb0706ae8526d077f3b8533f59c530c40f357
David Condrey <davidcondrey@me.com>
2016-10-16 04:05:00 -0700

    removes redundant sudo

--- osx.sh
+++ osx.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 
 # ~/osx.sh — Originally from https://mths.be/osx
-
-# Ask for the administrator password upfront
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until `osx.sh` has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
 ###############################################################################
 # General UI/UX                                                               #
 ###############################################################################
