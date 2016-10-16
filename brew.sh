b2318f5188f80d3a8a044d328b95f8d583fade6a
David Condrey <davidcondrey@me.com>
2016-10-16 05:01:59 -0700

    updates to docker-toolbox

--- brew.sh
+++ brew.sh
@@ -142,26 +142,29 @@ brew install caskroom/cask/brew-cask
 brew tap caskroom/versions
 
 # Core casks
-brew cask install --appdir="/Applications" alfred
-brew cask install --appdir="~/Applications" iterm2
-brew cask install --appdir="~/Applications" java
-brew cask install --appdir="~/Applications" xquartz
+echo 'export HOMEBREW_CASK_OPTS="--appdir=/Applications --caskroom=/etc/Caskroom"' >>~/.bash_profile
+source $HOME/.bash_profile
+
+brew cask install alfred
+brew cask install iterm2
+brew cask install java
+brew cask install xquartz
 
 # Development tool casks
-brew cask install --appdir="/Applications" sublime-text3
-brew cask install --appdir="/Applications" atom
-brew cask install --appdir="/Applications" virtualbox
-brew cask install --appdir="/Applications" vagrant
-brew cask install --appdir="/Applications" macdown
+brew cask install sublime-text3
+brew cask install atom
+brew cask install virtualbox
+brew cask install vagrant
+brew cask install macdown
 
 # Misc casks
-brew cask install --appdir="/Applications" google-chrome
-brew cask install --appdir="/Applications" firefox
-brew cask install --appdir="/Applications" skype
-brew cask install --appdir="/Applications" slack
-brew cask install --appdir="/Applications" dropbox
-brew cask install --appdir="/Applications" evernote
-brew cask install --appdir="/Applications" 1password
+brew cask install google-chrome
+brew cask install firefox
+brew cask install skype
+brew cask install slack
+brew cask install dropbox
+brew cask install evernote
+brew cask install 1password
 #brew cask install --appdir="/Applications" gimp
 #brew cask install --appdir="/Applications" inkscape
 
@@ -172,8 +175,7 @@ brew cask install --appdir="/Applications" 1password
 brew cask alfred link
 
 # Install Docker, which requires virtualbox
-brew install docker
-brew install boot2docker
+brew install docker-toolbox
 
 # Install developer friendly quick look plugins; see https://github.com/sindresorhus/quick-look-plugins
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
