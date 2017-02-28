e3bc63f350e6a21a1752e0a3c27f775e4e6b3c13
Jean-Francois Zinque <jzinque@gmail.com>
2017-02-28 16:57:18 +0800

    Change bootstrap.sh to exclude .sh files
    bootstrap.sh copied all the installation files to the home directory.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ cd "$(dirname "${BASH_SOURCE}")";
 git pull origin master;
 
 function doIt() {
-    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "*.sh" \
         --exclude "README.md" --exclude "LICENSE" -avh --no-perms . ~;
     source ~/.bash_profile;
 }
