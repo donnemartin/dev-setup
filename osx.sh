6a6fabce6242f49b3ffb8776ffdd572d42131368
Joseph Milla <josephmilla@users.noreply.github.com>
2017-04-02 17:17:34 -0700

    Update osx.sh

--- osx.sh
+++ osx.sh
@@ -9,7 +9,7 @@ osascript -e 'tell application "System Preferences" to quit'
 # Ask for the administrator password upfront
 sudo -v
 
-# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+# Keep-alive: update existing `sudo` time stamp until `osx.sh` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 ###############################################################################
