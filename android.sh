7f81c58ead6bf9a5f714e73bd1296b865d08ff57
Thor <thorsan@users.noreply.github.com>
2018-03-18 13:45:49 +0100

    Remove redundant cask install from android.sh (#75)

--- android.sh
+++ android.sh
@@ -18,12 +18,8 @@ fi
 # Make sure we’re using the latest Homebrew.
 brew update
 
-# Install Cask
-brew install caskroom/cask/brew-cask
-brew tap caskroom/versions
-
 brew cask install --appdir="~/Applications" java
-brew cask install --appdir="~/Applications" Caskroom/versions/intellij-idea-ce
+brew cask install --appdir="~/Applications" intellij-idea-ce
 brew cask install --appdir="~/Applications" android-studio
 
 brew install android-sdk
