0414f9bfce3dcbc3006b5bb7d79bc89e693ff4cd
Ota Mares <ota@rebuy.com>
2018-03-28 00:19:48 +0200

    Use rcm for dotfile management

---
+++ rcm.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Check for Homebrew,
+# Install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+brew tap thoughtbot/formulae
+brew install rcm
+
+# setup dotfiles
+env RCRC=".rcrc" rcup
