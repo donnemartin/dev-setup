570d9a95cbfd33f3ce9cecbcc9001785455b2ea6
Erik <43416374+errrrk@users.noreply.github.com>
2018-10-23 12:27:51 +0200

    Remove betterzipql since it doesn't exist
    Removed betterzipql since it's not available as cask and it subsequently doesn't install any of the other casks either

--- brew.sh
+++ brew.sh
@@ -168,7 +168,7 @@ brew install docker
 brew install boot2docker
 
 # Install developer friendly quick look plugins; see https://github.com/sindresorhus/quick-look-plugins
-brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv qlimagesize webpquicklook suspicious-package
 
 # Remove outdated versions from the cellar.
 brew cleanup
