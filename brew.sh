0c9f0a56b3c2a4925d7a0dc4fee482f7e002062e
Etty <beltrami.ester@gmail.com>
2019-04-05 15:25:33 -0700

    Fix brew install gnu-sed --with-default-names invalid option
    option --with-default-names was deprecated
    https://github.com/Homebrew/homebrew-core/commit/8c4f31e6643469493fc26d275be09cb05b09beac

--- brew.sh
+++ brew.sh
@@ -31,7 +31,7 @@ brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
+brew install gnu-sed
 # Install Bash 4.
 brew install bash
 brew tap homebrew/versions
