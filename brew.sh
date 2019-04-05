be156a4f7c7d0e72a6c3b82d443d959ee3f54501
Etty <beltrami.ester@gmail.com>
2019-04-05 15:50:13 -0700

    bew tap homebrew/version deprecated
    The command output:
    Error: homebrew/versions was deprecated. This tap is now empty as all its formulae were migrated.
    now it's the default
    https://github.com/dasilvacontin/libfreenect2/commit/613af9cd4605b04424624ac8c357fa2b0b288f0e

--- brew.sh
+++ brew.sh
@@ -4,7 +4,7 @@
 
 # Ask for the administrator password upfront.
 sudo -v
-Fix brew install gnu-sed --with-default-name invalid option
+
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
